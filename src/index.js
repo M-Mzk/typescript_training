77756b8c6826e835d29febc04567006f3a6abe0b
mukai <mizu.m.cl@gmail.com>
2026-03-17 21:53:49 +0900

    Fix .gitignore to remove tracking of ignored files

--- src/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const username = "Meiyu";
-const age = 23;
-const createSelfIntroduction = (name, age) => {
-    return `My name is ${name}, I'm ${age} years old.`;
-};
-const selfIntroduction = createSelfIntroduction(username, age);
-console.log(selfIntroduction);
